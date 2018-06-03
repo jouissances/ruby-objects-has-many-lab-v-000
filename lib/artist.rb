09eeeb0f50e72d48ba25296551f871842890b3a2
Janice Darikho <janicedarikho@gmail.com>
2018-06-03 04:48:13 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,12 @@
 class Artist
   attr_accessor :name
   
-  @@songs = []
-  
   def initialize(name)
     @name = name
   end
   
   def songs
-    @@songs 
+    @songs 
   end
   
   def add_song(song)
